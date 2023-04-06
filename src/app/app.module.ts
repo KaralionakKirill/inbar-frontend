bbbe2542466785029a5fd6a6f90af57be77718eb
Korolenok Kirill <korolenok.kirill.w@gmail.com>
2023-04-06 14:41:43 +0300

    INBAR-1: Config project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { RouterOutlet } from '@angular/router'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterOutlet
   ],
   providers: [],
   bootstrap: [AppComponent]
