251f314c0209424e09e2464a59ccbe3b1406a7a0
Korolenok Kirill <korolenok.kirill.w@gmail.com>
2023-04-26 23:50:19 +0300

    INBAR-7: Update inner banner component

--- src/app/components/inner-page-banner/inner-page-banner.component.ts
+++ src/app/components/inner-page-banner/inner-page-banner.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-inner-page-banner',
   templateUrl: './inner-page-banner.component.html',
   styleUrls: ['./inner-page-banner.component.css']
 })
-export class InnerPageBannerComponent {
+export class InnerPageBannerComponent implements OnInit {
+
+  @Input() title!: string
+
+  links: { link: string, value: string }[] = []
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+    const urls = this.router.url.split('/').filter(value => value !== '')
+    for (let i = 0; i < urls.length; i++) {
+      if (i == 0) {
+        this.links.push({
+          link: `/${urls[i]}`,
+          value: this.getValue(urls[i])
+        })
+      } else {
+        this.links.push({
+          link: `${this.links[i - 1]}/${urls[i]}`,
+          value: this.getValue(urls[i])
+        })
+      }
+    }
+    console.log(this.links)
+  }
+
+  getValue(value: string) {
+    let result
+    switch (value) {
+      case 'ingredients': {
+        result = 'Ингредиенты'
+        break
+      }
+      case 'recipes': {
+        result = 'Рецепты'
+        break
+      }
+      case 'registration': {
+        result = 'Регистрация'
+        break
+      }
+      default: {
+        result = value
+      }
+    }
+    return result
+  }
 
 }
